70fa6f0ddb470f6de15f2d8e32703c586433069f
thefather12 <94747184+thefather12@users.noreply.github.com>
2023-10-17 18:40:28 -0600

    Add files via upload

---
+++ LINKS-LIBRARIES/SPR.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Soporte Remoto (SPR)
+echo "" >/dev/null 2>&1
+wget https://raw.githubusercontent.com/thefather12/VPS-SV/main/LINKS-LIBRARIES/HELP.sh -O /usr/bin/SOPORTE > /dev/null 2>&1
+chmod +x /usr/bin/SOPORTE > /dev/null 2>&1
